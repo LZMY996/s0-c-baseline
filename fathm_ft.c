e58941e726b56cfb9f2a2d4597c522222b547448
LZMY996 <ll2049307617@gmail.com>
2026-08-15 01:25:48 +0800

    Delete fathm_ft.c

--- fathm_ft.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void){
-    int feet,fathoms;
-    fathoms = 2;
-    feet = 6 * fathoms;                
-    printf("There are %d feet in %d fathoms!\n",feet,fathoms);
-    printf("Yes,I said %d feet!\n",6 * fathoms);                /*占位符可以给一个式子*/
-    return 0;
-}
