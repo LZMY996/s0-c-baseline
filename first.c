d2bc40409d3f942671abbc6b3c6916b3a22cd07d
LZMY996 <ll2049307617@gmail.com>
2026-08-14 20:54:54 +0800

    Add files via upload

---
+++ first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+    int num;
+    num = 1;
+    printf("I am a simple ");
+    printf("computer.\n");                                              /*\n 是换行符*/
+    printf("My favorite number is %d because it is first.", num);       /*%d 是整数占位符*/
+    return 0;
+}
